13db42448b6fb376313ef26e971cb4db2df8bdec
96tm <khabcity@gmail.com>
2021-11-13 23:54:41 +0300

    refactor: move main.js into a a file

--- my_ciphering_cli.js
+++ my_ciphering_cli.js
@@ -1,35 +1,3 @@
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import { APP_ERROR_CODE } from './src/utils/constants.js';
-import { CustomError } from './src/utils/custom-error.js';
-import CliParser from './src/input-parsing/parser.js';
-import actionToStream from './src/utils/action-to-stream.js';
-
-process.on('uncaughtExceptionMonitor', (err) => {
-  if (err instanceof CustomError) {
-    process.stderr.write(err.message + '\n');
-    process.exit(APP_ERROR_CODE);
-  }
-});
+import main from './src/main.js';
 
 main();
-
-export default function main() {
-  const parser = new CliParser();
-  const args = process.argv.slice(2).join(' ');
-  const { actions, inputStream, outputStream } = parser.parse(' ' + args);
-  const promisifyPipeline = promisify(pipeline);
-  promisifyPipeline(
-    inputStream,
-    ...actions.map((action) => {
-      return actionToStream(action, inputStream, outputStream);
-    }),
-    outputStream
-  )
-    .then(() => {
-      console.log('Finished');
-    })
-    .catch((err) => {
-      throw new CustomError(err.message);
-    });
-}
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { APP_ERROR_CODE } from './utils/constants.js';
+import { CustomError } from './utils/custom-error.js';
+import CliParser from './input-parsing/parser.js';
+import actionToStream from './utils/action-to-stream.js';
+
+process.on('uncaughtExceptionMonitor', (err) => {
+  if (err instanceof CustomError) {
+    console.error(err.message);
+    process.exit(APP_ERROR_CODE);
+  }
+});
+
+export default function main() {
+  const parser = new CliParser();
+  const args = process.argv.slice(2).join(' ');
+  const { actions, inputStream, outputStream } = parser.parse(' ' + args);
+  console.log(Object.is(inputStream, process.stdin));
+  const promisifyPipeline = promisify(pipeline);
+  promisifyPipeline(
+    inputStream,
+    ...actions.map((action) => {
+      return actionToStream(action, inputStream, outputStream);
+    }),
+    outputStream
+  )
+    .then(() => {
+      console.log('Finished');
+    })
+    .catch((err) => {
+      throw new CustomError(err.message);
+    });
+}
